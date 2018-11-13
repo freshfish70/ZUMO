8dc05fd8db2f9e16e7e0b0f2e371eee9b3fb2796
freshfish70 <christoffer.andersen@live.com>
2018-11-13 17:53:46 +0100

    yep big update

--- src/main.cpp
+++ src/main.cpp
@@ -45,22 +45,25 @@
 */
 #include <ZumoShield.h>
 
-const int DEBUG_REMOTE = true;
+const int DEBUGGING = true;
+
 // Lightreflection read threshhold; higher is darker.
-const int QTR_THRESHOLD = 1900; // time in microseconds
 
 // SPEEDS 0(min) - 400(max)
-const int REVERSE_SPEED = 400; // Wheel speed when reversing
-const int TURN_SPEED = 350;    // Wheel speed when turning
-const int FORWARD_SPEED = 300; // Wheel speed when driving forward
-const int SEARCH_SPEED = 280;  // Wheel speed when searching
-const int MAX_SPEED = 400;     // Max speed
+const int REVERSE_SPEED = 400; // Wheel speed when reversing : Default 400
+const int TURN_SPEED = 350;    // Wheel speed when turning : Default 350
+const int FORWARD_SPEED = 200; // Wheel speed when driving forward : Default 300
+const int SEARCH_SPEED = 300;  // Wheel speed when searching : Defaulr 280
+const int MAX_SPEED = 400;     // Max speed : Default 400
 
 // DURATIONS
-const int REVERSE_DURATION = 400; // How long it should drive backwards, time in milliseconds DEF: 400
-const int TURN_DURATION = 300;    // How many ms it shoukd spend to turn, time in milliseconds DEF : 300
+const int REVERSE_DURATION = 230; // How long it should drive backwards, time in milliseconds DEF: 400
+const int TURN_DURATION = 280;    // How many ms it shoukd spend to turn, time in milliseconds DEF : 300
+const int COUNTDOWN_TIME = 1;     // SECONDS
 
 // DISTANCES
+
+// 45 CM
 const int MAX_CHARGE_DISTANCE = 45; // The maximum distance in CM to the enemy that we should allow for a charge attack
 
 // DIGITAL OUTPUT
@@ -75,7 +78,13 @@ unsigned long searchSequenceTimeout;
 unsigned long noDetectionTimeout;
 unsigned long movingTimeout;
 
+// SHIFT REGISTER
+// const int LATCH_PIN = 6;
+// const int CLOCK_PIN = 2;
+// const int DATA_PIN = 11;
+
 // REFLECTION SENSOR ARRAY
+const int QTR_THRESHOLD = 1700;                                   // time in microseconds
 const unsigned char NUMBER_OF_SENSORS = 2;                        // How many sensors we are using
 const unsigned int SENSOR_READ_TIMEOUT = 2000;                    // How long the sensor should read before timing out
 const int LEFT_SENSOR_PIN = 4;                                    // Left reflection sensor
@@ -83,30 +92,51 @@ const int RIGHT_SENSOR_PIN = 5;                                   // Right refle
 unsigned char sensorPins[] = {LEFT_SENSOR_PIN, RIGHT_SENSOR_PIN}; // Sensor pins we are reading on
 unsigned int sensor_values[NUMBER_OF_SENSORS];                    // Used to hold sensor readout values
 
+const int ULTRASONIC_ECHO_PIN = 6;    // connects to the echo pin on the distance sensor
+const int ULTRASONIC_TRIGGER_PIN = 2; // connects to the trigger pin on the distance sensor
+
 // creats a ZumoReflectanceSensor array instance
 ZumoReflectanceSensorArray sensors(sensorPins, NUMBER_OF_SENSORS, SENSOR_READ_TIMEOUT, QTR_NO_EMITTER_PIN);
 ZumoBuzzer buzzer;              // creates a ZumoBuzzer object
 ZumoMotors motors;              // creates a ZumoMotor object
 Pushbutton button(ZUMO_BUTTON); // default Zumo Button - pin 12
 
-const int LATCH_PIN = 6;
-const int CLOCK_PIN = 2;
-const int DATA_PIN = 11;
+/*
+  Border colors
+*/
+enum BORDER_COLOR
+{
+  WHITE_BORDER,
+  BLACK_BORDER
+};
+const int BORDER_DETECT_COLOR = WHITE_BORDER;
 
+/*
+  Directions which the Sumo can turn
+  used to control turning
+  and keep track of which direction it is currently tuning or 
+  was turning.
+
+  creates turningDirection
+*/
 enum TURN_DIRECTION
 {
   RANDOM_DIRECTION,
   LEFT_DIRECTION,
   RIGHT_DIRECTION
-} turnDirection;
+} turningDirection;
 
-// Sensor trigger states
+/*
+  Triggered sensors
+  
+  creates triggeredReflectSensor
+*/
 enum TRIGGERED_SENSOR
 {
   NONE,
   LEFT_SENSOR,
   RIGHT_SENSOR
-} triggeredSensor;
+} triggeredReflectSensor;
 
 // STATES
 enum STATES
@@ -153,23 +183,13 @@ void driveBackwards(int reverseSpeed)
 }
 
 /*
-  Drive the Zumo backwards at provided speed
-
-  @Param reverseSpeed how fast the zumo should drive backwards
-*/
-void driveLeftBackwards(int reverseSpeed)
-{
-  motors.setSpeeds(-reverseSpeed, -(reverseSpeed / 2));
-}
-
-/*
   Turn the Zumo left with a provided speed
 
   @Param turnSpeed how fast the zumo should turn 
 */
 void turnLeft(int turnSpeed)
 {
-  turnDirection = LEFT_DIRECTION;
+  turningDirection = LEFT_DIRECTION;
   motors.setSpeeds(-turnSpeed, turnSpeed);
 }
 
@@ -180,7 +200,7 @@ void turnLeft(int turnSpeed)
 */
 void turnRight(int turnSpeed)
 {
-  turnDirection = RIGHT_DIRECTION;
+  turningDirection = RIGHT_DIRECTION;
   motors.setSpeeds(turnSpeed, -turnSpeed);
 }
 
@@ -190,6 +210,16 @@ void turnRight(int turnSpeed)
 void stopMotors()
 {
   motors.setSpeeds(0, 0);
+}
+
+/*
+  Set the triggered reflectance sensor 
+
+  @Param sensor that is triggered
+*/
+void setTriggeredReflectanceSensor(TRIGGERED_SENSOR sensor)
+{
+  triggeredReflectSensor = sensor;
 }
 
 /*
@@ -210,9 +240,33 @@ int getIRDistance(int sensorPin)
 }
 
 /*
+  Messures distance to object from HC-SR04 DISTANCE SENSOR in CM
+
+  @Param sensorTriggerPin the trigger pin on the sensor
+  @Param sensorEchoPin the echo pin on the sensor
+  @Retrun float messured distance in cm
+*/
+float getUltrasonicMessuredDistance(int sensorTriggerPin, int sensorEchoPin)
+{
+  float echoTime;           //variable to store the time it takes for a ping to bounce off an object
+  float calcualtedDistance; //variable to store the distance calculated from the echo time
+
+  //send out an ultrasonic pulse that's 10ms long
+  digitalWrite(sensorTriggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensorTriggerPin, LOW);
+
+  echoTime = pulseIn(sensorEchoPin, HIGH); //use the pulsein command to see how long it takes for the
+                                           //pulse to bounce back to the sensor which is a time in microseconds
+
+  calcualtedDistance = echoTime * 0.01715; //calculate the distance of the object that reflected the pulse (half the speed of sound converted to CM/microseconds) to get
+  return calcualtedDistance;               //send back the distance that was calculated
+}
+
+/*
   Reads the reflections sensors and determine
   if the Zumo is on the edge
-  if it is, set global triggeredSensor to the sensor that
+  if it is, set global triggeredReflectSensor to the sensor that
   detected the edge
 
   @Return bool true if on edge else false
@@ -224,75 +278,64 @@ bool isOnEdge()
   int leftSensorValue = sensor_values[0];
   int rightSensorValue = sensor_values[1];
   bool sensorIsTriggered = false;
+  TRIGGERED_SENSOR sensor = NONE;
 
-  if (leftSensorValue < QTR_THRESHOLD)
+  switch (BORDER_DETECT_COLOR)
   {
-    triggeredSensor = LEFT_SENSOR;
-    sensorIsTriggered = true;
+  case WHITE_BORDER:
+    if (leftSensorValue < QTR_THRESHOLD)
+    {
+      sensor = LEFT_SENSOR;
+    }
+    else if (rightSensorValue < QTR_THRESHOLD)
+    {
+      sensor = RIGHT_SENSOR;
+    }
+    break;
+  case BLACK_BORDER:
+    if (leftSensorValue > QTR_THRESHOLD)
+    {
+      sensor = LEFT_SENSOR;
+    }
+    else if (rightSensorValue > QTR_THRESHOLD)
+    {
+      sensor = RIGHT_SENSOR;
+    }
+    break;
   }
-  else if (rightSensorValue < QTR_THRESHOLD)
+  if (sensor != NONE)
   {
-    triggeredSensor = RIGHT_SENSOR;
     sensorIsTriggered = true;
+    setTriggeredReflectanceSensor(sensor);
   }
 
   return sensorIsTriggered;
 }
-
 /*
-  Checks which sensor triggered the edge check
-  and runs a movement sequence based on which sensor triggered sensor
-  and reset the triggered sensor
+  Set a timers timeout time in milliseconds
+
+  @Param *timer timer reference the timer to add time out on
+  @Param time how much time to add
 */
-void runEdgeEscapeSequence()
+void setTimeout(unsigned long *timer, float time)
 {
-  driveBackwards(REVERSE_SPEED);
-  delay(REVERSE_DURATION);
-  switch (triggeredSensor)
-  {
-  case LEFT_SENSOR:
-    turnRight(TURN_SPEED);
-    break;
-  case RIGHT_SENSOR:
-    turnLeft(TURN_SPEED);
-    break;
-  }
-  delay(TURN_DURATION);
-  driveForward(FORWARD_SPEED);
-  triggeredSensor = NONE;
+  *timer = millis() + time;
 }
 
 /*
-  Turns the Zumo rendomly right or left at speed set by SEARCH_SPEED
-*/
-void runSearchSequence(int speed, TURN_DIRECTION direction)
-{
-  int rnd = random(0, 2);
-  Serial.println("SEARCH");
-  switch (direction)
-  {
-  case LEFT_DIRECTION:
-    turnLeft(speed);
-    break;
-  case RIGHT_DIRECTION:
-    turnRight(speed);
-    break;
-  case RANDOM_DIRECTION:
-    (rnd > 1) ? turnLeft(speed) : turnRight(speed);
-    break;
-  }
-}
+  Check if passed timer has timed out
 
-/*
-  Check if we are on the edge
-  and run the escape sequence if we are
+  @Return bool true if expired
 */
-void edgeDetect()
+bool timerTimedOut(unsigned long timer)
 {
-  if (isOnEdge())
+  bool isExpired = false;
+  if (millis() > timer)
   {
-    runEdgeEscapeSequence();
+    isExpired = true;
   }
+
+  return isExpired;
 }
 
 /*
@@ -331,30 +374,67 @@ bool isOperationStateChanged()
 }
 
 /*
-  Set a timers timeout time in milliseconds
+  Turns the Zumo in given direction at givin speed
 
-  @Param *timer timer reference the timer to add time out on
-  @Param time how much time to add
+  @Param speed the speed to turn in
+  @Param direction the direction to turn in
 */
-void setTimeout(unsigned long *timer, float time)
+void turn(int speed, TURN_DIRECTION direction)
 {
-  *timer = millis() + time;
+
+  switch (direction)
+  {
+  case LEFT_DIRECTION:
+    turnLeft(speed);
+    break;
+  case RIGHT_DIRECTION:
+    turnRight(speed);
+    break;
+  case RANDOM_DIRECTION:
+  {
+    int rnd = random(0, 10);
+    (rnd > 5) ? turnLeft(speed) : turnRight(speed);
+    break;
+  }
+  }
 }
 
 /*
-  Check if passed timer has timed out
-
-  @Return bool true if expired
+  Checks which sensor triggered the edge check
+  and runs a movement sequence based on which sensor triggered sensor
+  and reset the triggered sensor
 */
-bool timerTimedOut(unsigned long timer)
+void runEdgeEscapeSequence()
 {
-  bool isExpired = false;
-  if (millis() > timer)
+  driveBackwards(REVERSE_SPEED);
+  delay(REVERSE_DURATION);
+  switch (triggeredReflectSensor)
   {
-    isExpired = true;
+  case LEFT_SENSOR:
+    turn(TURN_SPEED, RIGHT_DIRECTION);
+    break;
+  case RIGHT_SENSOR:
+    turn(TURN_SPEED, LEFT_DIRECTION);
+    break;
   }
 
-  return isExpired;
+  delay(TURN_DURATION);
+  driveForward(MAX_SPEED);
+  setOperationState(S_MOVING);
+  setTimeout(&movingTimeout, 350);
+  triggeredReflectSensor = NONE;
+}
+
+/*
+  Check if we are on the edge
+  and run the escape sequence if we are
+*/
+void borderDetect()
+{
+  if (isOnEdge())
+  {
+    runEdgeEscapeSequence();
+  }
 }
 
 /*
@@ -368,10 +448,11 @@ bool enemyInSight()
 {
   bool charge = false;
 
-  int irDistance = getIRDistance(FRONT_IR_SENSOR);
+  // int irDistance = getIRDistance(FRONT_IR_SENSOR);
+  int distance = getUltrasonicMessuredDistance(ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN);
 
-  Serial.println(irDistance);
-  if (irDistance <= MAX_CHARGE_DISTANCE)
+  Serial.println(distance);
+  if (distance <= MAX_CHARGE_DISTANCE)
   {
     charge = true;
   }
@@ -379,17 +460,30 @@ bool enemyInSight()
   return charge;
 }
 
-void lockTarget(int speed, TURN_DIRECTION wasTurningDirection, int duration = 100)
+/*
+  Tries too lock the target by reversing the search spin direction for a very
+  short period, to compansate for "sliding" when zumo stops turning at high speed before it charges.
+
+  @Param speed the speed on turn
+  @Param wasTurningDirection the direction the zumo was turning in when detected enemy
+  @Param turnDuration how long it should execute the reversed turn
+*/
+void lockTarget(int speed, TURN_DIRECTION wasTurningDirection, int turnDuration = 60)
 {
   switch (wasTurningDirection)
   {
   case LEFT_DIRECTION:
-    turnRight(speed);
+    turn(speed, RIGHT_DIRECTION);
     break;
   case RIGHT_DIRECTION:
-    turnLeft(speed);
+    turn(speed, LEFT_DIRECTION);
+    // turnLeft(speed);
+    break;
+  case RANDOM_DIRECTION:
+    turnDuration = 0;
     break;
   }
+  delay(turnDuration);
 }
 
 /*
@@ -419,40 +513,39 @@ void shiftOut(int dataPin, int clockPin, int bitOrder, uint16_t value)
 /*
   
 */
-void shift(double power, bool single = false)
-{
-  int minus;
-  (single == true) ? minus = 0 : minus = 1;
-
-  digitalWrite(LATCH_PIN, LOW);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, pow(2, power) - minus);
-  digitalWrite(LATCH_PIN, HIGH);
-}
+// void shift(double power, bool single = false)
+// {
+//   int minus;
+//   (single == true) ? minus = 0 : minus = 1;
+//   digitalWrite(LATCH_PIN, LOW);
+//   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, pow(2, power) - minus);
+//   digitalWrite(LATCH_PIN, HIGH);
+// }
 
 /*
   Waits for start button press and
   then runs a count down sequence
-  before changing state to S_SEARCHING
 
+  @Param seconds how many seconds we should count down for
 */
-void waitForStartButtonAndCountDown()
+void waitForStartButtonAndCountDown(int seconds)
 {
   digitalWrite(LED_PIN, HIGH);
   button.waitForButton();
   digitalWrite(LED_PIN, LOW);
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < seconds; i++)
   {
     // shift(2 * (5 - i));
     delay(1000);
     buzzer.playNote(NOTE_C(5), 200, 20);
   }
   // shift(10);
-  buzzer.playNote(NOTE_C(5), 1000, 20);
+  // buzzer.playNote(NOTE_C(5), 1000, 20);
 }
 
 /*
-  Reads serial for incomming commands.
-
+  Reads serial for incomming keyboard commands
+  Used when debugging
 */
 void readSerial()
 {
@@ -494,37 +587,34 @@ void readSerial()
   }
 }
 
-////////////////////////////////////////////////////////
+///////////////////  LOOP & SETUP  /////////////////////////
 
 void setup()
 {
   Serial.begin(9600);
 
-  triggeredSensor = NONE;         // Set initial triggered sensor
+  triggeredReflectSensor = NONE;  // Set initial triggered sensor
   currentOperationState = S_IDLE; // Sets initial state
   pinMode(LED_PIN, HIGH);         // Turn signal LED on
 
   randomSeed(analogRead(0)); // Make sure our random seed is different each time we run
 
-  pinMode(LATCH_PIN, OUTPUT);
-  pinMode(DATA_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(ULTRASONIC_ECHO_PIN, INPUT);
+  pinMode(ULTRASONIC_TRIGGER_PIN, OUTPUT);
 }
 
 void loop()
 {
 
-  if (button.isPressed())
-  {
-    setOperationState(S_IDLE);
-  }
+  bool operationStateChanged = isOperationStateChanged();
 
-  // HUSK Å TA DENNE AV PÅ TOPPEN NÅR VI SKAL TIL KAMP / KONKURANSE!!!!!
-  if (DEBUG_REMOTE)
-  {
-    readSerial();
-  }
+  /*
 
+  */
+  if (currentOperationState != S_IDLE)
+  {
+    borderDetect();
+  }
   /*
     STATE MACHINE
   */
@@ -535,15 +625,18 @@ void loop()
     {
       stopMotors();
       button.waitForRelease();
-      waitForStartButtonAndCountDown();
+      waitForStartButtonAndCountDown(COUNTDOWN_TIME);
       setOperationState(S_SEARCHING);
     }
     break;
   case S_SEARCHING:
-    edgeDetect(); // CHECK IF ZUMO IS ON EDGE
-    if (isOperationStateChanged())
+
+    /*
+      Set start values for this state
+    */
+    if (operationStateChanged)
     {
-      setTimeout(&noDetectionTimeout, 2500);
+      setTimeout(&noDetectionTimeout, random(2200, 2700));
       setTimeout(&searchSequenceTimeout, 0);
       setLastOperationState(S_SEARCHING);
     }
@@ -552,16 +645,16 @@ void loop()
     {
       if (enemyInSight())
       {
-        lockTarget(300, turnDirection, 60);
+        // lockTarget(300, turningDirection, 40);
         driveForward(MAX_SPEED);
         setOperationState(S_CHARGING);
       }
-      setTimeout(&frontSensorTimeout, 40);
+      setTimeout(&frontSensorTimeout, 30);
     }
 
     if (timerTimedOut(searchSequenceTimeout))
     {
-      runSearchSequence(SEARCH_SPEED, RANDOM_DIRECTION);
+      turn(SEARCH_SPEED, RANDOM_DIRECTION);
       setTimeout(&searchSequenceTimeout, 1000);
     }
 
@@ -574,7 +667,6 @@ void loop()
 
     break;
   case S_MOVING:
-    edgeDetect();
 
     if (timerTimedOut(movingTimeout))
     {
@@ -585,7 +677,7 @@ void loop()
     {
       if (enemyInSight())
       {
-        lockTarget(300, turnDirection, 60);
+        // lockTarget(300, turningDirection, 40);
         driveForward(MAX_SPEED);
         setOperationState(S_CHARGING);
       }
@@ -593,9 +685,11 @@ void loop()
     }
     break;
   case S_CHARGING:
-    edgeDetect(); // CHECK IF ZUMO IS ON EDGE
-    // Serial.println("CHARGE");
-    if (isOperationStateChanged())
+
+    /*
+      Set start values for this state
+    */
+    if (operationStateChanged)
     {
       setTimeout(&frontSensorTimeout, 300);
       driveForward(MAX_SPEED);
@@ -606,12 +700,23 @@ void loop()
     {
       if (!enemyInSight())
       {
-        runSearchSequence(340, turnDirection);
+        turn(340, turningDirection);
         setTimeout(&searchSequenceTimeout, 1000);
         setOperationState(S_SEARCHING);
       }
       setTimeout(&frontSensorTimeout, 50);
     }
     break;
+  }
+
+  if (button.isPressed())
+  {
+    setOperationState(S_IDLE);
+  }
+
+  // HUSK Å TA DENNE AV PÅ TOPPEN NÅR VI SKAL TIL KAMP / KONKURANSE!!!!!
+  if (DEBUGGING)
+  {
+    readSerial();
   }
 }
